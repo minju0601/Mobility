aa6d7665a10aa9f2deda418ae6b6445b8f9215c3
minju0601 <anny06010425@gmail.com>
2025-05-23 08:53:13 +0900

    echo "# sosujapangi" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git push -u origin main

--- Main.c
+++ Main.c
@@ -1,7 +1,3 @@
-echo "# sosujapangi" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin 
-git push -u origin main
+int add(int a, int b) { 
+    return a + b;
+}
