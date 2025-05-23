b8ba5383567d8ffec5ce8a5b331c933cc352836f
minju0601 <anny06010425@gmail.com>
2025-05-23 11:27:28 +0900

    Notion에서 Github 연동 확인 코드드

--- Main.c
+++ Main.c
@@ -1,3 +1,23 @@
 int add(int a, int b) { 
     return a + b;
 }
+
+
+int main() {
+    int a, b, result;
+
+    // 사용자 입력
+    printf("첫 번째 정수를 입력하세요: ");
+    scanf("%d", &a);
+
+    printf("두 번째 정수를 입력하세요: ");
+    scanf("%d", &b);
+
+    // 곱셈
+    result = a * b;
+
+    // 결과 출력
+    printf("%d 곱하기 %d는 %d입니다.\n", a, b, result);
+
+    return 0;
+}
